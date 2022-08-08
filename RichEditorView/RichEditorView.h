6c94dae2353f0d9f59afea57aeb46b96a7f26279
trudy <trudy@appknot.com>
2022-08-08 17:11:44 +0900

    CNG: RichEditorView 오류 수정

--- RichEditorView/RichEditorView.h
+++ RichEditorView/RichEditorView.h
@@ -6,16 +6,16 @@
 //
 //
 
-#import <UIKit/UIKit.h>
-
-#import <RichEditorView/CJWWebView+HackishAccessoryHiding.h>
-
-//! Project version number for RichEditorView.
-FOUNDATION_EXPORT double RichEditorViewVersionNumber;
-
-//! Project version string for RichEditorView.
-FOUNDATION_EXPORT const unsigned char RichEditorViewVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <RichEditorView/PublicHeader.h>
-
-
+//#import <UIKit/UIKit.h>
+//
+//#import <RichEditorView/CJWWebView+HackishAccessoryHiding.h>
+//
+////! Project version number for RichEditorView.
+//FOUNDATION_EXPORT double RichEditorViewVersionNumber;
+//
+////! Project version string for RichEditorView.
+//FOUNDATION_EXPORT const unsigned char RichEditorViewVersionString[];
+//
+//// In this header, you should import all the public headers of your framework using statements like #import <RichEditorView/PublicHeader.h>
+//
+//
